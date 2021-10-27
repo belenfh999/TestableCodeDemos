09bb2eaa7b7c24a826d34135650a5c6cc44d4ff5
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-10-27 16:58:50 +0200

    Module 2: Implemented Database (it only reads a file)

--- Module2/common/database.rb
+++ Module2/common/database.rb
@@ -1,7 +1,18 @@
 module Module2
   class Database < IDatabase
     def get_invoice(invoice_id)
-
+      invoice = Invoice.new
+      line_index = 0
+      File.foreach("../invoices/#{invoice_id}.txt") do |each_line|
+        case line_index
+        when 0
+          invoice.id = each_line
+        when 1
+          invoice.total = each_line.to_f
+        when 2
+          invoice.date = each_line
+        end
+      end
     end
   end
 end
--- Module2/common/invoice.rb
+++ Module2/common/invoice.rb
@@ -1,5 +1,5 @@
 module Module2
   class Invoice
-    attr_accessor :id, :total
+    attr_accessor :id, :total, :date
   end
 end
