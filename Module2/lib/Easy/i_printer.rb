d87d7eda8f4bb6d928cb0ad013381752fc8383e1
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-10-27 16:26:49 +0200

    Module 2: Added easy to test printer class

---
+++ Module2/lib/Easy/i_printer.rb
@@ -0,0 +1,7 @@
+module Module2
+  class IPrinter
+    def write_line(txt)
+      raise 'Don\'t forget to implement the write_line method'
+    end
+  end
+end
---
+++ Module2/lib/Easy/printer.rb
@@ -0,0 +1,7 @@
+module Module2
+  class Printer < IPrinter
+    def write_line(text)
+      puts text
+    end
+  end
+end
