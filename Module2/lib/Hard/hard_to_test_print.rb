ac171098f0ac76432cb403a2c895dc24524eb644
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-10-28 09:29:40 +0200

    Added working implementation for PrinterInvoiceCommand execute

--- Module2/lib/Hard/hard_to_test_print.rb
+++ Module2/lib/Hard/hard_to_test_print.rb
@@ -1,19 +1,26 @@
+require_relative '../../common/invoice'
+require_relative '../../common/database'
+require_relative './hard_to_test_printer'
+
 module Module2
   class PrintInvoiceCommand
     def execute(invoice_id)
       database = Database.new
-
+      database.file_path = "../../invoices/#{invoice_id}.txt"
       invoice = database.get_invoice(invoice_id)
 
-      HardToTestPrinter.write_line("Invoice ID: #{invoice.id}")
+      printer = HardToTestPrinter.new
+      printer.write_line("Invoice ID: #{invoice.id}")
 
-      HardToTestPrinter.write_line("Total: $#{invoice.total}")
+      printer.write_line("Total: $#{invoice.total}")
 
       date_time = Time.now
 
-      HardToTestPrinter.write_line("Printed: #{date_time.to_s}")
+      printer.write_line("Printed: #{date_time.to_s}")
     end
   end
+  p = PrintInvoiceCommand.new
+  p.execute('inv_1')
 end
 
 
