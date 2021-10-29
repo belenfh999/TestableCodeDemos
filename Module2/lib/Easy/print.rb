2a2d5d752cbcdb80fbb6270ad5fccbf5c9764c5e
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-10-29 08:44:17 +0200

    Added testable print

---
+++ Module2/lib/Easy/print.rb
@@ -0,0 +1,30 @@
+require_relative '../../common/database'
+require_relative '../../common/invoice'
+require_relative './printer'
+require_relative './date_time_wrapper'
+module Module2
+  class PrintInvoiceCommand
+    attr_reader :database, :printer, :date_time
+
+    def initialize(database, printer, date_time)
+      @database = database
+      @printer = printer
+      @date_time = date_time
+    end
+
+    def execute(invoice_id)
+      invoice = @database.get_invoice(invoice_id)
+
+      @printer.write_line("Invoice ID: #{invoice.id}")
+
+      @printer.write_line("Total: $#{invoice.total}")
+
+      @printer.write_line("Printed: #{@date_time.to_s}")
+    end
+  end
+  db = Database.new
+  invoice_id = 'inv_1'
+  db.file_path = "../../invoices/#{invoice_id}.txt"
+  p = PrintInvoiceCommand.new(db, Printer.new, DateTimeWrapper.new)
+  p.execute(invoice_id)
+end
