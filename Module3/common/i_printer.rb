2a1e73cf09fcba68a1552433f97563bd625acc1e
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-11-10 09:09:03 +0100

    Module 3: Added functionality to printer (page layout)

--- Module3/common/i_printer.rb
+++ Module3/common/i_printer.rb
@@ -1,5 +1,13 @@
 module Module3
   class IPrinter
+    def set_page_layout(layout)
+      raise 'Don\'t forget to implement the set_page_layout method'
+    end
+
+    def set_ink_color(color)
+      raise 'Don\'t forget to implement the write_line method'
+    end
+
     def write_line(txt)
       raise 'Don\'t forget to implement the write_line method'
     end
