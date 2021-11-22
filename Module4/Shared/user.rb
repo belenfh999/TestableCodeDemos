cf88cf4605605d4b61a89412ab251a2a8e1575d1
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-11-22 09:11:18 +0100

    Module 4: Added User and tests

---
+++ Module4/Shared/user.rb
@@ -0,0 +1,9 @@
+module Module4
+  class User
+    attr_reader :username
+
+    def initialize(username)
+      @username = username
+    end
+  end
+end
---
+++ Module4/test/user_test.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+
+require_relative '../Shared/user'
+
+class UserTest < Minitest::Test
+  include Module4
+
+  def test_create_user
+    expected_username = 'a-username'
+
+    user = User.new(expected_username)
+
+    assert_equal(expected_username, user.username)
+  end
+
+  def test_cannot_change_username
+    initial_name = 'a-username'
+    modified_username = 'another-username'
+
+    user = User.new(initial_name)
+
+    assert_raises NoMethodError do
+      user.username = modified_username
+    end
+  end
+
+
+end
+
