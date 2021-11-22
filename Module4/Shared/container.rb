a3a01c65ea54f77e7ebb32d62218c9ee403b99ce
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-11-22 09:46:29 +0100

    Module 4: Added container to module

--- Module4/Shared/container.rb
+++ Module4/Shared/container.rb
@@ -1,19 +1,21 @@
-class Container
-  def initialize
-    @c   = {}
-    @fac = {}
-    yield self
-  end
+module Module4
+  class Container
+    def initialize
+      @c   = {}
+      @fac = {}
+      yield self
+    end
 
-  def [] k
-    @c[k] || (@fac[k] && @fac[k].call(self))
-  end
+    def [] k
+      @c[k] || (@fac[k] && @fac[k].call(self))
+    end
 
-  def []= k, v
-    @c[k] = v
-  end
+    def []= k, v
+      @c[k] = v
+    end
 
-  def factory k, &b
-    @fac[k] = b
+    def factory k, &b
+      @fac[k] = b
+    end
   end
 end
