aa0a7464f99c7d44a17729f55c3df609b06365df
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-11-25 14:45:10 +0100

    Module 5: Added database

---
+++ Module5/Shared/i_database.rb
@@ -0,0 +1,11 @@
+module Module5
+  class IDatabase
+    def get_invoice(invoice_id)
+      raise 'Don\'t forget to implement the get_invoice method'
+    end
+
+    def save
+      raise 'Don\'t forget to implement the save method'
+    end
+  end
+end
