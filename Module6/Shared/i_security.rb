713f7f46f42de78a77cb967c4cf10922cfce8453
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-12-17 08:55:04 +0100

    Module 6: Added security class

---
+++ Module6/Shared/i_security.rb
@@ -0,0 +1,11 @@
+module Module6
+  class ISecurity
+    def get_username
+      raise 'Don\'t forget to implement the get_username method'
+    end
+
+    def is_admin
+      raise 'Don\'t forget to implement the is_admin method'
+    end
+  end
+end
---
+++ Module6/Shared/security.rb
@@ -0,0 +1,20 @@
+require_relative './i_security'
+
+module Module6
+  class Security < ISecurity
+    attr_reader :username, :is_admin
+
+    def set_user(username, is_admin)
+      @username = username
+      @is_admin = is_admin
+    end
+
+    def get_username
+      @username
+    end
+
+    def is_admin
+      @is_admin
+    end
+  end
+end
