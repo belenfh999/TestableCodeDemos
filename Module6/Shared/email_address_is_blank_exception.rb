182cc67fa4302c4e56b035775d43b4daceb945c1
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-12-17 08:50:22 +0100

    Module 6: Added email valid implementation

--- Module6/Shared/email_address_is_blank_exception.rb
+++ Module6/Shared/email_address_is_blank_exception.rb
@@ -1,1 +1,6 @@
-
+class EmailAddressIsBlankException < StandardError
+  def initialize(msg="A blank email is not valid.", exception_type="custom")
+    @exception_type = exception_type
+    super(msg)
+  end
+end
--- Module6/Shared/email_validator.rb
+++ Module6/Shared/email_validator.rb
@@ -1,8 +1,21 @@
 require_relative './i_email_validator'
+require_relative './email_address_is_blank_exception'
+
 module Module6
   class EmailValidator < IEmailValidator
+    attr_accessor :emails
+
+    def initialize(file_path)
+      @emails = []
+      file = File.open(File.expand_path("emails.txt", File.dirname(file_path)))
+      file.each do |each_line|
+        @emails.push(each_line.strip)
+      end
+    end
+
     def valid?(address)
-      raise 'Don\'t forget to implement the valid? method'
+      raise EmailAddressIsBlankException if address == ""
+      @emails.include? address
     end
   end
 end
